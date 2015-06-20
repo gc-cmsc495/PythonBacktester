5f387e5f5442265afdf0a46c47fab36abf982dfb
gc-cmsc495 <garrett@garrettcasey.com>
2015-06-20 15:31:28 -0400

    Create Analysis.py

---
+++ Analysis.py
@@ -0,0 +1,15 @@
+# Date: 2015-06-20
+#
+# Description:
+
+# The Analysis class gets the trade data from TradeManager
+# and calculate statistics for each indicator/markout_length pair.
+# Statistics include: mean return, standard deviation, t-statistc or sharpe ratio,
+# cumulative return, max and min returns, skew, kurtosis and other statistics.
+
+# Class Author: Justin Kelley
+
+class Analysis(object):
+
+    def __init__(self):
+        return True
