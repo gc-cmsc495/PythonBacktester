825644f4323ed884c36ac1ff9ebf0abc261b81ef
gc-cmsc495 <garrett@garrettcasey.com>
2015-06-20 15:28:31 -0400

    Create TradeManager.py

---
+++ TradeManager.py
@@ -0,0 +1,16 @@
+# Date: 2015-06-20
+#
+# Description:
+#
+# The TradeManager class will use the Data class and Indicator Library
+# to iterate through the sample period, record the instances when the
+# indicators are true, determine the future prices, and ensure events
+# are independent by ensuring no trade overlapping.
+# 
+
+# Class Author: Garrett Casey
+
+class TradeManager(object):
+
+    def __init__(self):
+        return True
