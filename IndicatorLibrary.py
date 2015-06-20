03e61361a75a875b702dd871145e317f2f8ba658
gc-cmsc495 <garrett@garrettcasey.com>
2015-06-20 15:24:43 -0400

    Create IndicatorLibrary.py

---
+++ IndicatorLibrary.py
@@ -0,0 +1,16 @@
+# Date: 2015-06-20
+#
+# Description:
+#
+# An indicator is a Boolean function that consumes a series of 
+# price or volume information from the Data Class.
+# Some indicators may be composites of other indicators
+
+# Class Author: Pierce Baker
+
+import pandas as pd
+
+class IndicatorLibrary(object):
+
+    def __init__(self):
+        return True
