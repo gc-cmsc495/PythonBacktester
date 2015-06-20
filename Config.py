4ea2c6e03c3adffce916d39aade5860691410594
gc-cmsc495 <garrett@garrettcasey.com>
2015-06-20 15:17:12 -0400

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,17 @@
+# Date: 2015-06-20
+# Description:
+# The user will execute the program via the command line with three arguments:
+# a start date, end date and the path to a text configuration file. The purpose of 
+# the config file to so that back-tests (experiments) can be reproduced simply
+# by providing the same config.
+
+# The Config class will parse the command line arguments and also the
+# configuration file. See example.ini for an example configuration file.
+
+# Class Author: Chris Owens
+
+
+class Config(object):
+
+    def __init__(self, path_to_config_file):
+        return True
