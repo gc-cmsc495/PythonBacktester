5112c8291723d618f28310543fbd342691e702d1
gc-cmsc495 <garrett@garrettcasey.com>
2015-06-20 15:33:11 -0400

    Create ReportWriter.py

---
+++ ReportWriter.py
@@ -0,0 +1,17 @@
+# Date: 2015-06-20
+#
+# Description:
+#
+# The report class will be responsible for taking results from the Analysis Class
+# and other classes and output the results in a well formatted text file.
+#
+# We must carefully consider the format in which the results are published so they
+# can be easily processed by other programs or utilities (e.g. Strategy Comparison Tools, GUIs, etc.).
+# 
+
+# Class Author: TBD
+
+class ReportWriter(object):
+
+    def __init__(self):
+        return True
