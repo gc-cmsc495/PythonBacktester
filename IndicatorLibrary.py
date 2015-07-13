c0a6d797b393a506884bb662cb40d3c81f6b7605
Diabl0658 <piercebaker@hotmail.com>
2015-07-12 23:00:48 -0400

    Update IndicatorLibrary.py
    Added more indicators, how many should we have?

--- IndicatorLibrary.py
+++ IndicatorLibrary.py
@@ -46,6 +46,21 @@ class Indicator(object):
         
     def calc(self, DataManager, ticker, date):
         return True
+
+class New_20_Period_Closing_High(Indicator):
+
+    def __init__(self):
+        Indicator.__init__(self)
+        self.max_history = 20
+
+    def calc(self, DataManager, ticker, date):
+        quote_list = DataManager.get(ticker, date, -20)
+        quote = quote_list.pop()
+        is_new_high = True
+        for q in quote_list[-20:]:
+            if quote.close < q.close:
+                is_new_high = False
+        return is_new_high
         
 class High_Volume(Indicator):
 
@@ -59,7 +74,52 @@ class High_Volume(Indicator):
         hist_volume = []
         for q in quote_list[-20:]:
             hist_volume.append(q.volume)
-        return quote.volume > mean(hist_volume)
+        return quote.volume > mean(hist_volume) * 2
+
+class New_52_Week_Low(Indicator):
+
+    def __init__(self):
+        Indicator.__init__(self)
+        self.max_history = 260
+
+    def calc(self, DataManager, ticker, date):
+        quote_list = DataManager.get(ticker, date, -260)
+        quote = quote_list.pop()
+        is_new_low = True
+        for q in quote_list[-260:]:
+            if quote.close > q.close:
+                is_new_low = False
+        return is_new_low
+
+class New_52_Week_High(Indicator):
+
+    def __init__(self):
+        Indicator.__init__(self)
+        self.max_history = 260
+
+    def calc(self, DataManager, ticker, date):
+        quote_list = DataManager.get(ticker, date, -260)
+        quote = quote_list.pop()
+        is_new_high = True
+        for q in quote_list[-260:]:
+            if quote.close < q.close:
+                is_new_high = False
+        return is_new_high
+
+class Crossed_Above_200d_Moving_Average(Indicator):
+
+    def __init__(self):
+        Indicator.__init__(self)
+        self.max_history = 200
+
+    def calc(self, DataManager, ticker, date):
+        quote_list = DataManager.get(ticker, date, -200)
+        quote_today = quote_list.pop()
+        quote_yesterday = quote_list.pop()
+        hist_close = []
+        for q in quote_list[-200:]:
+            hist_close.append(q.close)
+        return quote_today.close > mean(hist_close) & quote_yesterday.close < mean(hist_close)
 
 class Close_Higher_Than_Open(Indicator):
 
@@ -69,15 +129,31 @@ class Close_Higher_Than_Open(Indicator):
     def calc(self, DataManager, ticker, date):
         quote = DataManager.get(ticker, date)
         return quote.close > quote.open
-     
+
+def new_new_20_period_closing_high():
+    return New_20_Period_Closing_High()
+
 def new_high_volume():
     return High_Volume()
+
+def new_new_52_week_low():
+    return New_52_Week_Low()
+
+def new_new_52_week_high():
+    return New_52_Week_High()
+
+def new_crossed_above_200d_moving_average():
+    return Crossed_Above_200d_Moving_Average()
 
 def new_close_higher_than_open():
     return Close_Higher_Than_Open()
 
 registered_indicators = {
+    'NEW_20_PERIOD_CLOSING_HIGH' : new_new_20_period_closing_high,
     'HIGH_VOLUME' : new_high_volume,
+    'NEW_52_WEEK_LOW' : new_new_52_week_low,
+    'NEW_52_WEEK_HIGH' : new_new_52_week_high,
+    'CROSSED_ABOVE_200D_MOVING_AVERAGE' : new_crossed_above_200d_moving_average,
     'CLOSE_HIGHER_THAN_OPEN' : new_close_higher_than_open,
 }
 
